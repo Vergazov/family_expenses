df987738b1f3edbace14737a898576409d0fecd1
Vergazov_A <vergazka@gmail.com>
2023-07-22 12:22:37 +0300

    добавил форму

--- family_expenses/resources/views/add_finances.blade.php
+++ family_expenses/resources/views/add_finances.blade.php
@@ -1,3 +1,16 @@
+<div class="container">
+    <form>
+        <div class="mb-3">
+            <label for="expenses" class="form-label">Статья расхода</label>
+            <input type="text" class="form-control" id="expenses">
+        </div>
+        <div class="mb-3">
+            <label for="sum" class="form-label">Сумма</label>
+            <input type="number" class="form-control" id="sum">
+        </div>
+        <button type="submit" class="btn btn-primary">Добавить</button>
+    </form>
+</div>
 @extends('layouts.main')
-добавление новой статьи
+
 
