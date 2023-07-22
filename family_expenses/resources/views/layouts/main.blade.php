278d42855f60ac003123a64a9b084422c0c2cc52
Vergazov_A <vergazka@gmail.com>
2023-07-22 11:09:39 +0300

    подключил бутстрап локально

--- family_expenses/resources/views/layouts/main.blade.php
+++ family_expenses/resources/views/layouts/main.blade.php
@@ -5,14 +5,28 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <title>Document</title>
 </head>
 <nav>
     <div>
+        <!-- As a link -->
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
 
+        <!-- As a heading -->
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+            </div>
+        </nav>
     </div>
 </nav>
 <body>
 @yield('content')
 </body>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
 </html>
